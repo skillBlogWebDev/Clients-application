82ce68aa7fc6e46c56c34623dba3d920f6b18dd6
Negan-ai <yoosyoos32@gmail.come>
2021-12-26 20:02:42 +0300

    create client item

--- client/js/createClientItem.js
+++ client/js/createClientItem.js
@@ -1,4 +1,7 @@
+import { createContactItemByType, formatDate, formatTime } from "./utils.js";
+
 export const createClientItem = (data) => {
+    console.log(data);
     const clientTr = document.createElement('tr');
     const clientId = document.createElement('span');
     const clientFullName = document.createElement('td');
@@ -15,13 +18,40 @@ export const createClientItem = (data) => {
     const clientActions = document.createElement('td');
     const clientEdit = document.createElement('button');
     const clientDelete = document.createElement('button');
-    
+
+    clientTr.classList.add('clients__item');
+    clientTr.id = data.id;
+    clientId.classList.add('client__id');
+    clientFullName.classList.add('clients__full-name');
+    clientName.classList.add('clients__name');
+    clientSurname.classList.add('clients__surname');
+    clientLastName.classList.add('clients__lastname');
+    clientCreated.classList.add('clients__created');
+    createDate.classList.add('created__date');
+    createdTime.classList.add('created__time');
+    clientChanged.classList.add('clients__changed');
+    changedDate.classList.add('changed__date');
+    changedTime.classList.add('changed__time');
+    clientContacts.classList.add('clients__contacts');
+    clientActions.classList.add('clients__actions');
+    clientContacts.classList.add('clients__contacts');
+    clientDelete.classList.add('clients__delete', 'btn-reset');
+    clientEdit.classList.add('clients__edit', 'btn-reset');
+
+    for (const contact of data.contacts) {
+        createContactItemByType(contact.type, contact.value, clientContacts);
+    }
+ 
     clientId.textContent = data.id.substr(0, 6);
     clientName.textContent = data.name;
     clientSurname.textContent = data.surname;
     clientLastName.textContent = data.lastName;
     clientEdit.textContent = 'Изменить';
     clientDelete.textContent = 'Удалить';
+    createDate.textContent = formatDate(data.createdAt);
+    createdTime.textContent = formatTime(data.createdAt);
+    changedDate.textContent = formatDate(data.updatedAt);
+    changedTime.textContent = formatTime(data.updatedAt);
 
     clientFullName.append(clientName, clientSurname, clientLastName);
     clientCreated.append(createDate, createdTime);
---
+++ client/js/utils.js
@@ -0,0 +1,72 @@
+import { svgPhone, svgFb, svgVk, svgEmail, svgOther } from "./svg.js";
+
+export const createContactLink = (type, value, element, svg, item) => {
+    element = document.createElement('a');
+    element.classList.add('contacts__link');
+    element.innerHTML = svg;
+
+    if (type === 'Email') {
+        element.href = `mailto:${value.trim()}`
+    } else if (type === 'Телефон') {
+        element.href = `tel:${value.trim()}`
+    } else {
+        element.href = value.trim();
+    }
+
+    item.append(element);
+}
+
+export const createContactItemByType = (type, value, item) => {
+    switch (type) {
+        case 'Телефон':
+            let phone;
+            createContactLink(type, value, phone, svgPhone, item);
+            break;
+        case 'Facebook':
+            let fb;
+            createContactLink(type, value, fb, svgFb, item);
+            break;
+        case 'VK':
+            let vk;
+            createContactLink(type, value, vk, svgVk, item); 
+            break;
+        case 'Email':
+            let email;
+            createContactLink(type, value, email, svgEmail, item); 
+            break;
+        case 'Email':
+            let other;
+            createContactLink(type, value, other, svgOther, item);  
+            break;      
+                    
+        default:
+            break;
+    }
+}
+
+export const formatDate = data => {
+    const newDate = new Date(data);
+
+    const correctDate = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    }
+
+    const resultDate = newDate.toLocaleString('ru', correctDate);
+
+    return resultDate;
+}
+
+export const formatTime = data => {
+    const newDate = new Date(data);
+
+    const correctDate = {
+        hour: 'numeric',
+        minute: 'numeric',
+    }
+
+    const resultTime = newDate.toLocaleString('ru', correctDate);
+
+    return resultTime;
+}
